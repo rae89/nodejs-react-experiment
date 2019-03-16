dd0f1fdc7b7afa1a9506b234bae74e95cefb633b
rae89 <raespinoza109@gmail.com>
2019-03-16 14:25:32 -0700

    fix typo

--- server/client/src/setupProxy.js
+++ server/client/src/setupProxy.js
@@ -1,4 +1,4 @@
-const proxy = require('http-procy-middleware');
+const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(proxy('/auth/google', { target: 'http:localhost:5000'}));
