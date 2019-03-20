865699fdb0bde9df5e928e2f69d0d0bdab0960d3
rae89 <raespinoza109@gmail.com>
2019-03-19 21:29:20 -0700

    update payment ui

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -5,3 +5,10 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const handleToken = token => {
+  return async (dispatch) => {
+    const res = await axios.post('/api/stripe', token);
+    dispatch({ type: FETCH_USER, payload: res.data });
+  };
+}
