f04fc87ddb73b04706e215cdb5b8a5d386d3f01f
rae89 <raespinoza109@gmail.com>
2019-03-19 21:59:39 -0700

    add billingRoutes

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -7,8 +7,8 @@ export const fetchUser = () => async dispatch => {
 };
 
 export const handleToken = token => {
-  return async (dispatch) => {
-    const res = await axios.post('/api/stripe', token);
+  return async dispatch => {
+    const res = await axios.post("/api/stripe", token);
     dispatch({ type: FETCH_USER, payload: res.data });
   };
-}
+};
