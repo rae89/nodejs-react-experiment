a5c78ac58556bee4c2db66ed23a64e2ce3623cf3
rae89 <raespinoza109@gmail.com>
2019-03-26 22:44:49 -0700

    rename Surveys to Survey

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
-require('./models/Surveys')
+require('./models/Survey')
 require("./services/passport");
 
 
