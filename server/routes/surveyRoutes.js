a8ee360748b98d0110935c054f1fa6924e4080d1
rae89 <raespinoza109@gmail.com>
2019-03-31 21:11:07 -0700

    add thanks api route to surveys routes

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -3,9 +3,15 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
+
 const Survey = mongoose.model('surveys');
 
-module.exports = (app) => {
+
+module.exports = app => {
+    app.get('/api/surveys/thanks', (req, res) => {
+      res.send('Thanks for voting!');
+    });
+
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
