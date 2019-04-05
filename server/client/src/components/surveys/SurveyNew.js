746d485f5bcd4052d47bfbcfb9a488acee84a2fc
rae89 <raespinoza109@gmail.com>
2019-04-04 20:21:50 -0700

    setup redux form and field componenets

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,12 @@
+// SurveyNew shows SurveyForm and SurveyForm Review
 import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
 
 class SurveyNew extends Component {
     render() {
         return (
             <div>
-                SurveyNew
+                <SurveyForm />
             </div>
         );
     }
