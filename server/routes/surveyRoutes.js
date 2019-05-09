35e2a215eca1c1dc422198ec0399e9b4a63d06c7
rae89 <raespinoza109@gmail.com>
2019-05-08 20:05:02 -0700

    odds and ends around surveys

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -35,7 +35,8 @@ module.exports = app => {
           },
           {
             $inc: { [choice]: 1 },
-            $set: { 'recipients.$.responded': true }
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
           }).exec();
         })
         .value();
